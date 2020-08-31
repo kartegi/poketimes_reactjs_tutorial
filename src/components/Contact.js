0ffa0556c355b84131e8078de36bb1e27e38753d
kartegi <kocmoc006@gmail.com>
2020-08-31 23:27:54 +0300

    tutorial

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = (props) => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet perferendis beatae iure dolor consequatur voluptatibus, necessitatibus corrupti ipsam, vel facere maxime culpa autem similique. Culpa vel sit molestiae temporibus quia.</p>
+        </div>
+    )
+}
+
+export default Contact
